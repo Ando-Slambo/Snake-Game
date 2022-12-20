11141ae3c9153928781892d232eac95eb3299bb9
Adam Johnson <jadam0303@gmail.com>
2022-12-20 02:32:48 -0500

    formatted code and comments

--- Engine/Snake.cpp
+++ Engine/Snake.cpp
@@ -14,43 +14,38 @@ void Snake::Initialize()
 
 void Snake::MoveHead(Keyboard& kbd)
 {
-	//left
-	if (kbd.KeyIsPressed(0x25) && !(direction == 2)) {
-		direction = 0;
-	}
-	//up
-	if (kbd.KeyIsPressed(0x26) && !(direction == 3)) {
-		direction = 1;
-	}
-	//right
-	if (kbd.KeyIsPressed(0x27) && !(direction == 0)) {
-		direction = 2;
-	}
-	//down
-	if (kbd.KeyIsPressed(0x28) && !(direction == 1)) {
-		direction = 3;
-	}
+	//all directions are from perspective of user,
+	// from snake's perspective it always turns right
+	
+	//left (not allowed if moving right)
+	if (kbd.KeyIsPressed(0x25) && !(direction == 2)) { direction = 0; }
+	//up (not allowed if moving down)
+	if (kbd.KeyIsPressed(0x26) && !(direction == 3)) { direction = 1; }
+	//right (not allowed if moving left)
+	if (kbd.KeyIsPressed(0x27) && !(direction == 0)) { direction = 2; }
+	//down (not allowed if moving up)
+	if (kbd.KeyIsPressed(0x28) && !(direction == 1)) { direction = 3; }
 
 	MoveBody(x, y);
 
 	if (direction == 0) {
-		//turn snake up if hit wall
+		//turn up if hit wall
 		if (x - 1 == 0) { direction = 1; }
 		x -= 1;
 	}
 	if (direction == 1) {
-		//turn snake right if hit wall
+		//turn right if hit wall
 		if (y - 1 == 0) { direction = 2; }
 		y -= 1;
 	}
 	if (direction == 2) {
-		//turn snake down if hit wall
+		//turn down if hit wall
 		if (x + 1 == Board::cells_x) { direction = 3; }
 		x += 1;
 	}
 	if (direction == 3) {
 		y += 1;
-		//turn snake left if hit wall
+		//turn left if hit wall
 		if (y == Board::cells_y) {
 			x -= 1;
 			direction = 0;
@@ -72,7 +67,7 @@ void Snake::Draw(Board& brd, Graphics& gfx)
 void Snake::MoveBody(int head_x, int head_y)
 {
 	//putting head co-ords into segment's array for the 0th segment to move to
-	//(head will move to new co-ords as soon as this function terminates)
+	// (head will move to new co-ords as soon as this function terminates)
 	xs[0] = head_x;
 	ys[0] = head_y;
 
@@ -94,10 +89,9 @@ bool Snake::BodyCollision()
 {
 	for (int i = 0; i < segments; i++) {
 		//check if head's co-ords are in the segment arrays and return true if so
-		//(only want to return false after checking every element)
+		// (only want to return false after checking every element)
 		if (x == segs[i].x && y == segs[i].y) { return true; }
 	}
-	//else return false
 	return false;
 }
 
